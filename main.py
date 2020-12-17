cc370d03d1baa7668fcd3c0447b21497d596b182
elver <contact@fastweb.com>
2020-12-17 17:46:23 -0500

    origins

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ origins = ["http://localhost.tiangolo.com",
             "https://localhost.tiangolo.com",
             "http://localhost", 
             "http://localhost:8080",
+            "https://g1m2-14-sprint3.herokuapp.com/",
             ]
 
 app.add_middleware(
